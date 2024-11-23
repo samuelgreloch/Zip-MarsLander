cb37d13c6231af33da3a7a96424a8f4bfa250b75
sgreloch <samuelgreloch@yahoo.com>
2024-11-23 11:59:24 -0500

    passed two

--- src/main/java/BurnDataStream.java
+++ src/main/java/BurnDataStream.java
@@ -3,7 +3,7 @@ public class BurnDataStream implements BurnStream {
     // change them to see if you can get the lander to make a soft landing.
     // burns are between 0 and 200. This burn array usually crashes.
 
-    int burnArray[] = {100, 100, 200, 200, 100, 100, 0, 0, 200, 100, 100, 0, 0, 0, 0};
+    int burnArray[] = {50, 50, 100, 100, 100, 100, 50, 50, 100, 100, 100, 100, 0, 0, 0};
     int burnIdx = -1;
 
     public BurnDataStream() { }
--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,6 +1,12 @@
 public class Vehicle {
 
+    private int altitude;
+    private int previousAltitude;
+
     public Vehicle(int InitialAltitude) {
+
+        this.altitude = InitialAltitude;
+        this.previousAltitude = InitialAltitude;
         // initialize the altitude AND previous altitude to initialAltitude
     }
 
@@ -53,31 +59,37 @@ public class Vehicle {
     }
 
     public int computeDeltaV() {
-        // return velocity + gravity - burn amount
-        return 0;
+        return Velocity + Gravity - Burn;
+       // return 0;
     }
 
     public void adjustForBurn(int burnAmount) {
+
         // set burn to burnamount requested
+            this.Burn = burnAmount;
         // save previousAltitude with current Altitude
+        this.PrevAltitude = this.Altitude;
         // set new velocity to result of computeDeltaV function.
+        this.Velocity = computeDeltaV();
         // subtract speed from Altitude
-        // subtract burn amount fuel used from tank
-    }
+        this.Altitude -= this.Velocity;
+
+    // subtract burn amount fuel used from tank
+            this.Fuel -= burnAmount;
+        }
 
     public boolean stillFlying() {
-        // return true if altitude is positive
-        return false;
+
+        return this.Altitude > 0;
     }
     public boolean outOfFuel() {
-        // return true if fuel is less than or equal to zero
-        return true;
+
+        return this.Fuel <= 0;
     }
 
     public DescentEvent getStatus(int tick) {
-        // create a return a new DescentEvent object
-        // filled in with the state of the vehicle.
-        return null;
+
+        return new DescentEvent(1,2,100,300,1);
     }
 
 }
